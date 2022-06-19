c8af091e1093042a574ed7284465460aa4647912
Mata <taiagomata@gmail.com>
2022-06-19 14:18:32 -0300

    Last Linux

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -21,13 +21,13 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps){
         try{
             const response = await api.post('/subscribe')
 
-            const { sessionID } = response.data;
+            const { sessionId } = response.data;
 
             const stripe = await getStripeJs()
 
-            await stripe.redirectToCheckout(sessionID)
+            await stripe.redirectToCheckout({ sessionId })
         }catch (err) {
-            console.log(err.message)
+            alert(err.message)
         }
         
     }
